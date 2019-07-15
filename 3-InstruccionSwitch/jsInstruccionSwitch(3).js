c69b72d3ac8b351282ced8f8de196f8307574c91
Dante Mogni <52411075+dantemogni@users.noreply.github.com>
2019-07-15 11:24:12 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -2,7 +2,24 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
-
+switch(mesDelAño)
+{
+    case "Enero":
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+    case "Junio":
+    case "Julio":
+    case "Agosto":
+    case "Septiembre":
+    case "Octubre":
+    case "Noviembre":
+    case "Diciembre":
+        alert("Este mes tiene 30 días o más");
+        break;
+    case "Febrero":
+        alert("Este mes no tiene más de 29 días");
+}
 //alert (mesDelAño);
 	
 	
