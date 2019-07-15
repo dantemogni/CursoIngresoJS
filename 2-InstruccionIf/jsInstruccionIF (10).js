3565d1475760041387c682e4f2279e30e134bb1c
Dante Mogni <52411075+dantemogni@users.noreply.github.com>
2019-07-15 09:50:33 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,19 @@
 function mostrar()
 {
+	var notas;
 	//Genero el número RANDOM entre 1 y 10 
-	
+	notas = Math.floor(Math.random() * 10) + 1;
+	if(notas >= 9)
+	{
+		alert("EXCELENTE, nota " + notas);
+	}
+	if(notas >=4 && notas < 9)
+	{
+		alert ("APROBÓ, nota: " + notas);
+	}
+	if(notas < 4)
+	{
+		alert("Vamos, la proxima se puede, nota: " + notas);
+	}
 
 }//FIN DE LA FUNCIÓN
