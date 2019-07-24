f1663f84aa2896a3efed41ae09c1cebaf4d21634
dantemogni <dantejoaquin.mogni@gmail.com>
2019-07-24 19:53:29 -0300

    tp resuelto

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,57 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+    var numUno;
+    var numDos;
+    var numTres;
+    var suma;
+    
+    numUno = document.getElementById("PrecioUno").value;
+    numUno = parseInt(numUno);
+    numDos = document.getElementById("PrecioDos").value;
+    numDos = parseInt(numDos);
+    numTres = document.getElementById("PrecioTres").value;
+    numTres = parseInt(numTres);
+
+    suma = numUno + numDos + numTres;
+    alert("La suma es " + suma);
 }
 function Promedio () 
 {
-	
+	var numUno;
+    var numDos;
+    var numTres;
+    var promedio;
+    
+    numUno = document.getElementById("PrecioUno").value;
+    numUno = parseInt(numUno);
+    numDos = document.getElementById("PrecioDos").value;
+    numDos = parseInt(numDos);
+    numTres = document.getElementById("PrecioTres").value;
+    numTres = parseInt(numTres);
+
+    promedio = (numUno + numDos + numTres)/3;
+
+    alert("El promedio es "+ promedio);
 }
 function PrecioFinal () 
 {
-	
+	var numUno;
+    var numDos;
+    var numTres;
+    var iva;
+    var suma;
+    var preciofinal;
+    
+    numUno = document.getElementById("PrecioUno").value;
+    numUno = parseInt(numUno);
+    numDos = document.getElementById("PrecioDos").value;
+    numDos = parseInt(numDos);
+    numTres = document.getElementById("PrecioTres").value;
+    numTres = parseInt(numTres);
+
+    suma = numUno + numDos + numTres;
+    iva = suma * 0.21;
+    preciofinal = suma + iva;
+    alert("El precio final es " + preciofinal);
 }
