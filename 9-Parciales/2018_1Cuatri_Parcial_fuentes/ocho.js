6d2676bdc7b14ad380d9290b5ed527356d92f177
Dante Mogni <52411075+dantemogni@users.noreply.github.com>
2019-08-05 12:30:30 -0300

    correcciones en clase

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,73 +1,70 @@
-function mostrar()
-{
+function mostrar() {
     var letra;
     var num;
-    var rta = true;
+   // var rta = true; // no es necesario poner esta variable, solo se reemplaza en el while() el confirm
     var contador = 0;
     var cantPares = 0;
     var cantImpares = 0;
     var cantCeros = 0;
-    var positivos=0;
-    var negativos=0;
+    var positivos = 0;
+    var negativos = 0;
     var cantPosi = 0;
-    var min = 0;
-    var max = 0;
+    var min;
+    var max;
     var letraMax;
     var letraMin;
-    var promedio;
-    
-    while(rta == true)
-    {
-        letra = prompt("Ingrese una letra:"); 
-    while(!(isNaN(letra)))
-        {
-            letra = prompt("Error, por favor ingrese una letra");
-        }
-    num = prompt("Ingrese un número: " + (contador + 1));
-    num = parseInt(num);
-    while(isNaN(num) || num < -100 || num > 100) 
-        {
+    var promedio=0; // si no se hacer la cuenta, muestra cero
+    var flag = 0;
+    do {
+        letra = prompt("Ingrese una letra:");
+        // while (!(isNaN(letra))) // me descarta los numeros pero no los simbolos o espacios en blanco, asi que tecnicamente no sirve
+        // {
+        //     letra = prompt("Error, por favor ingrese una letra");
+        // }
+        num = prompt("Ingrese un número: " + (contador + 1));
+        num = parseInt(num);
+        while (isNaN(num) || num < -100 || num > 100) {
             num = prompt("Error, por favor ingrese un número entre -100 y 100");
         }
-        rta = confirm("¿Desea seguir ingresando datos?");
-    
-    if(num %2 == 0 && num !=0)
-    {
-        cantPares++;
-    } else if (num %2 != 0) {
-        cantImpares++;
-    } else {
-        cantCeros++;
     }
+    while (confirm("¿Desea seguir ingresando datos?"))
+    {
 
-    if(num > 0)
-    {
-        positivos = num + positivos; // sumo los posi
-        cantPosi++; // cuento los posi
-    } else {
-        negativos = num + negativos; //sumo los nega
-        //negativos++; 
-    }
+        if (num % 2 == 0 && num != 0) {
+            cantPares++;
+        } else if (num % 2 != 0) {
+            cantImpares++;
+        } else {
+            cantCeros++;
+        }
 
-    if(contador==0)
-    {
-        min = num;
-        max = num;
-    }
-    if(num > max)
-    {
-        max = num;
-        letraMax = letra; 
-    } 
-    if(num < min) {
-        min = num;
-        letraMin = letra;
-    }
-    
-    contador++;
-}
+        if (num > 0) {
+            positivos = num + positivos; // sumo los posi (acumulador posi)
+            cantPosi++; // cuento los posi
+        } else {
+            negativos = num + negativos; //sumo los nega (acumulador nega)
+            //negativos++; 
+        }
 
+        if (contador == 0) {
+            min = num;
+            max = num;
+        }
+        if (num > max || flag == 0) {
+            max = num;
+            letraMax = letra;
+        }
+        if (num < min || flag == 0) {
+            min = num;
+            letraMin = letra;
+            flag=1; // ver comentario abajo 
+        }
+
+        contador++;
+    }
+    if(cantPosi != 0) { // si no hay numeros positivos, no se puede hacer la division
     promedio = positivos / cantPosi;
+    }
 
     document.write("La cantidad de números pares es " + cantPares + "<br>");
     document.write("La cantidad de números impares es de " + cantImpares + "<br>");
@@ -77,3 +74,21 @@ function mostrar()
     document.write("El número y la letra del máximo es " + max + " " + letraMax + " y el número y la letra del mínimo es " + min + " " + letraMin + "<br>");
 
 }
+// los maximos y minimos se pueden tambien asi:
+// var flag=0;
+//var mayor;
+//var menor;
+//..
+//..
+//..
+//if(num > mayor || flag == 0)
+//{
+//  mayor=num;    
+//} 
+//if(num < menor || flag ==0)
+// {
+//  menor=num;
+//  flag = 1;           (esto hace que a partir de aca flag sea falso y no se ejecute por asi decirlo, entonces solo se fija en la primera condicion)   
+//}
+
+// alert("hola" + "\n chau") devuelve un hola y abajo el chau
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,43 +1,45 @@
-function mostrar()
-{
+function mostrar() {
     var notas;
     var sexo;
     var contador = 0;
     var promedio;
-    var sumatotal = 0;
-    var min = +Infinity;
+    var sumatotal = 0; //esto es un ACUMULADOR
+    var min;
     var sexmin;
     var cantVaron = 0;
- 
-    while(contador < 5)
-    {
-           notas = prompt("Ingresar la nota: " + (contador+1));
-            notas=parseInt(notas);
-           sumatotal = sumatotal + notas;
-           contador++;
-            while(!(notas >= 0 && notas <= 10)) 
-        {
+    for(var i = 0; i<5; i++) {
+        notas = prompt("Ingresar la nota: " + (contador + 1));
+        notas = parseInt(notas);
+        //mejor asi:
+        //notas=parseInt(prompt(Ingrese nota: ""));
+        sumatotal = sumatotal + notas;
+        // o sumatotal += notas;
+        contador++;
+        while (!(notas >= 0 && notas <= 10)) {
             notas = prompt("Por favor ingrese notas en el rango de 0 a 10.");
+            //ACA DE VUELTA EL PARSE INT
         }
-            sexo = prompt("Ingresar el sexo del alumno:");
-            while(!(sexo == "f" || sexo == "m"))
+        sexo = prompt("Ingresar el sexo del alumno:");
+        //sexo = sexo.toLowerCase();
+        //se puede poner asi: sexo=prompt("Ingresar sexo").toLowerCase();
+        while (!(sexo == "f" || sexo == "m"))
+        //while(sexo != "f" && sexo != "m")
         {
             sexo = prompt("Ingrese un sexo válido.");
+            //va devuelta el lowercase
         }
-        if(notas < min) 
-        {
-            min = notas;
-            sexmin = sexo;
+        if (notas < min || i==0) { // ??? creo q es porque notas > min solo me da undefined porque no se sabe que es "min", 
+            min = notas;           // entonces ejecuta el i==0 que es verdadero, conviertiendo el "min" en el numero.
+            sexmin = sexo;         // entonces cuando se vuelve a ejecutar el codigo (es una iteracion), el min ya tiene un valor
+        }                          // entonces ya no da màs undefined y listo
+        if (notas >= 6 && sexo == "m") {
+            cantVaron++;
         }
-        if(notas >=6 && sexo == "m")
-         {
-             cantVaron++;
-         }
     }
-      
-   promedio = sumatotal/5;
-   alert("El promedio es " + promedio);
-   alert("La nota mínima es " + min + " y pertenece al sexo: " + sexmin);
-   alert("La cantidad de varones con nota mayor o igual a 6 es: " + cantVaron);
+
+    promedio = sumatotal / 5;
+    alert("El promedio es " + promedio);
+    alert("La nota mínima es " + min + " y pertenece al sexo: " + sexmin);
+    alert("La cantidad de varones con nota mayor o igual a 6 es: " + cantVaron);
 
 }
