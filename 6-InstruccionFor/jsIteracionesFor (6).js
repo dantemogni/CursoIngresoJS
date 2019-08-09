166ec8f8a711f5059122bbda16f529c672583b45
dantemogni <dantejoaquin.mogni@gmail.com>
2019-08-08 21:50:14 -0300

    resuelto

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,7 +1,13 @@
 function mostrar()
 {
-
-
+ var num = parseInt(prompt("Ingrese un número"));
+ var pares=0;
+ var cantpares=0;
+ for(; pares % 2 == 0 && pares <= num; pares++ && cantpares++) {
+     alert(pares++);
+     
+ }
+alert("La cantidad de pares es de: " + cantpares++);
 
 
 }//FIN DE LA FUNCIÓN
